353baf0f9a93b32896337fa92a589d3d6ade8c05
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-31 22:16:32 +0200

    [UPD] BONUS 2 completato

--- script.js
+++ script.js
@@ -37,11 +37,18 @@ const teamMembers = [
   }
 ];
 
+// reupero gli elementi dal dom
+const name = document.getElementById('name')
+const role = document.getElementById('role')
+const email = document.getElementById('mail')
+const image = document.getElementById('image')
+const teamMembersAdd = document.getElementById('team-members')
+const newMember = document.getElementById('newMember')
 // creo la funzione che mi permette di creare le singole card
 
 const cardCreator = (member) => {
   const card = `<div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-    <div class="card-container d-flex align-items-center bg-dark">
+    <div class="card-container d-flex align-items-center bg-black">
       <div class="img-team">
         <img src="${member.img}" alt="male1" class="img-fluid">
       </div>
@@ -52,7 +59,8 @@ const cardCreator = (member) => {
       </div>
     </div>
   </div >`
-  document.getElementById('team-members').innerHTML += card
+  document.getElementById('team-members').innerHTML += card;
+  return card;
 }
 // creo un ciclo che inserisce le informazioni di ciascun membro
 // all'interno della sua crad
@@ -60,3 +68,29 @@ for (let i = 0; i < teamMembers.length; i++) {
   cardCreator(teamMembers[i])
 }
 
+// creo un evento per far si che al click del bottone 
+// venga aggiunta una nuova card nel team
+
+newMember.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const nameValue = name.value;
+  const roleValue = role.value;
+  const emailValue = email.value;
+  const imageValue = image.value;
+  //verifico che vengano compilati tutti i campi
+  if (!nameValue || !roleValue || !emailValue || !imageValue) {
+    alert('Devi compilare tutti i campi')
+    return;
+  }
+  // creo il nuovo oggetto
+  const newMember = {
+    name: nameValue,
+    role: roleValue,
+    email: emailValue,
+    image: imageValue
+  }
+  teamMembers.push(newMember)
+ 
+  cardCreator(newMember)
+})
